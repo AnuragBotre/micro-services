1f8aee474c436aabc54197a236bb83212c1e692e
anurag <anurag.botre.202@gmail.com>
2021-01-16 21:06:00 +0530

    Added config server.

--- cloud-gateway/src/main/java/com/trendcore/cloud/gateway/CloudGatewayRunner.java
+++ cloud-gateway/src/main/java/com/trendcore/cloud/gateway/CloudGatewayRunner.java
@@ -2,12 +2,14 @@ package com.trendcore.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
+@EnableConfigServer
 public class CloudGatewayRunner {
 
     public static void main(String[] args) {
--- user-service/src/main/java/com/trendcore/user/UserServiceRunner.java
+++ user-service/src/main/java/com/trendcore/user/UserServiceRunner.java
@@ -3,10 +3,13 @@ package com.trendcore.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceRunner {
 
     public static void main(String[] args) {
